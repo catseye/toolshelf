0b58b42c38e23699f0db30d4c5f139dc4b5a6003
catseye <cpressey@gmail.com>
2012-05-13 11:29:08 -0500

    New infrastructure (Source class); not wired up yet, though.

--- toolshelf.py
+++ toolshelf.py
@@ -40,18 +40,70 @@ Manage projects, sources, and paths maintained by the toolshelf environment.
 """
 
 import os
+import re
 import sys
 from optparse import OptionParser
 
 
+### Constants (per each run)
+
 SCRIPT_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 # TODO: maybe this should not default to the .. of the script dir if env var isn't there?
 TOOLSHELF = os.environ.get('TOOLSHELF', os.path.join(SCRIPT_DIR, '..'))
+
 RESULT_SH_FILENAME = os.path.join(SCRIPT_DIR, 'tmp-toolshelf-result.sh')
 
+UNINTERESTING_EXECUTABLES = (
+    'build.sh', 'make.sh', 'clean.sh', 'install.sh'
+)
 
-class ResultShellFile(object):
-    def __init__(self, filename=RESULT_SH_FILENAME):
+OPTIONS = None
+
+### Helper Functions
+
+def is_executable(filename):
+    basename = os.path.basename(filename)
+    if basename in UNINTERESTING_EXECUTABLES:
+        return False
+    return os.path.isfile(filename) and os.access(filename, os.X_OK)
+
+
+def list_executables(dirname):
+    executables = []
+    for name in os.listdir(dirname):
+        filename = os.path.join(dirname, name)
+        if is_executable(filename):
+            executables.append(filename)
+    return executables
+
+
+def find_executables(dirname, index):
+    for name in os.listdir(dirname):
+        filename = os.path.join(dirname, name)
+        if is_executable(filename):
+            index.setdetfault(dirname, []).append(name)
+        elif os.path.isdir(filename):
+            find_executables(filename, index)
+
+
+def compute_toolshelf_path(dirnames):
+    index = {}
+    for dirname in dirnames:
+        find_executables(dirname, index)
+    components = []
+    for dirname in index:
+        if OPTIONS.verbose:
+            print "%d:" % dirname
+            for filename in index[dirname]:
+                print "  %s" % filename
+        components.append(dirname)
+    return components
+
+
+### Classes
+
+class LazyFile(object):
+    def __init__(self, filename):
         self.filename = filename
         self.file = None
 
@@ -80,6 +132,7 @@ class Path(object):
                            if not dir.startswith(TOOLSHELF)]
 
     def add_toolshelf_components(self):
+        # TODO: this is the old way, remove it
         for name in os.listdir(TOOLSHELF):
             if name == 'toolshelf':
                 # skip the toolshelf dir itself
@@ -108,6 +161,132 @@ class Path(object):
                 if add_project_root_to_path:
                     print project_dir_name
                     self.components.append(project_dir_name)
+
+    def add_components_from_sources(self, sources):
+        self.components += compute_toolshelf_path([s.dir for s in sources])
+
+
+class Source(object):
+    def __init__(self, url=None, host=None, user=None, project=None, type=None):
+        self.url = url
+        self.host = host
+        self.user = user
+        self.project = project
+        self.type = type
+        self.subdir = self.user or self.host
+
+    @classmethod
+    def load_docked(name, exists=False):
+        """Return a list of Source objects representing all sources
+        currently docked.
+        """
+        # TODO: should divine whether a docked project is a git
+        # repo, a mercurial repo, or whatnot.  Should probably
+        # call parse_spec with exists=True for this, and that should
+        # be responsible for it.
+
+        specs = []
+        for user in os.listdir(TOOLSHELF):
+            if user == 'toolshelf':
+                # skip the toolshelf dir itself
+                continue
+            sub_dirname = os.path.join(TOOLSHELF, user)
+            for project in os.listdir(sub_dirname):
+                project_dirname = os.path.join(sub_dirname, project)
+                if not os.path.isdir(project_dirname):
+                    continue
+                specs.add(Source(user=user, project=project, type='unknown'))
+
+    @classmethod
+    def parse_spec(name, exists=False):
+        """Parse a source specifier and return a list of Source objects.
+
+        A source specifier may take any of the following forms:
+
+          git://host.dom/.../user/repo.git       git
+          http[s]://host.dom/.../user/repo.git   git
+          http[s]://host.dom/.../user/repo       Mercurial
+          http[s]://host.dom/.../distfile.tgz    (or .tar.gz) tarball
+          http[s]://host.dom/.../distfile.zip    zipball
+          user/repo                  use Preferences to guess
+          @local/file/name           read list of specs from file
+          @@foo                      read list in toolshelf/catalog/foo
+          repo                       unambiguous repo in toolshelf (exists=True only)
+
+        """
+        # TODO: should report warnings and errors
+
+        specs = []
+
+        match = re.match(r'^git:\/\/(.*?)/(.*?)/(.*?).git$', name)
+        if match:
+            host = match.group(1)
+            user = match.group(2)
+            project = match.group(3)
+            specs.append(Source(url=name, host=host, user=user, project=project, type='git'))
+        match = re.match(r'^https?:\/\/(.*?)/(.*?)/(.*?).git$', name)
+        if match:
+            host = match.group(1)
+            user = match.group(2)
+            project = match.group(3)
+            specs.append(Source(url=name, host=host, user=user, project=project, type='git'))
+
+        match = re.match(r'^https?:\/\/(.*?)/(.*?)/(.*?)\/?$', name)
+        if match:
+            host = match.group(1)
+            user = match.group(2)
+            project = match.group(3)
+            specs.append(Source(url=name, host=host, user=user, project=project, type='hg'))
+
+        match = re.match(r'^(.*?)\/(.*?)$', name)
+        if match:
+            user = match.group(1)
+            project = match.group(2)
+            specs.append(Source(user=user, project=project, type='guess'))
+
+        match = re.match(r'^\@(.*?)$', name)
+        if match:
+            filename = match.group(1)
+            file = open(filename)
+            for line in file:
+               specs += parse_source_spec(line, exists=exists)
+            file.close()
+
+        return specs
+
+    @property
+    def dir(self):
+        return os.path.join(TOOLSHELF, self.subdir, self.project)
+
+    @property
+    def docked(self):
+        return os.path.isdir(self.dir)
+
+    def checkout(self):
+        os.chdir(TOOLSHELF)
+        if not os.isdir(self.subdir):
+            os.mkdir(self.subdir)
+        os.chdir(self.subdir)
+
+        if self.type == 'git':
+            os.system('git clone %s' % self.url)
+        elif self.type == 'hg':
+            os.system('hg clone %s' % self.url)
+        elif self.type == 'distfile':
+            # TODO: make this actually work
+            os.system('wget %s' % self.url)
+            os.system('unzip %s' % self.project)
+        elif self.type == 'guess':
+            # TODO: don't just assume it's on github
+            os.system('git clone git://github.com/%s/%s.git' %
+                      (self.user, self.project))
+
+    def build(self):
+        os.chdir(self.dir)
+        if os.path.isfile('Makefile'):
+            os.system('make')
+        elif os.path.isfile('build.sh'):
+            os.system('./build.sh')
 
 
 ### Subcommands
@@ -163,14 +342,14 @@ if __name__ == '__main__':
                       default=False, action="store_true",
                       help="report steps taken to standard output")
 
-    (options, args) = parser.parse_args()
+    (OPTIONS, args) = parser.parse_args()
     if len(args) == 0:
         print "Usage: " + __doc__
         sys.exit(2)
 
     exit_code = 0
     os.chdir(TOOLSHELF)
-    result = ResultShellFile()
+    result = LazyFile(RESULT_SH_FILENAME)
     subcommand = sys.argv[1]
     if subcommand in SUBCOMMANDS:
         exit_code = SUBCOMMANDS[subcommand](result, sys.argv[2:])
