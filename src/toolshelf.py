98609d3d10809b9bffecc5c0c77360611feddbec
Cat's Eye Technologies <cpressey@gmail.com>
2013-10-20 20:04:01 +0100

    Transplant `lint` functionality from Chrysoberyl into here.

--- src/toolshelf.py
+++ src/toolshelf.py
@@ -941,7 +941,6 @@ def release(args):
     sources = Source.from_specs(specs)
     for source in sources:
         distro = source.project
-        repo_dir = source.dir
         tag = source.get_latest_release_tag()
         if not tag:
             raise SystemError("Repository not tagged")
@@ -985,9 +984,87 @@ def release(args):
             command.append('-X')
             command.append(x)
         command.append(full_filename)
-        os.chdir(repo_dir)
+        os.chdir(source.dir)
         run(*command)
         os.chdir(cwd)
+
+
+def lint(args):
+    """Check that the layouts of distributions conform to some
+    distribution organization guidelines.  If these are not guidelines that
+    you use, you can take this with a grain of salt.
+
+    """
+    cwd = os.getcwd()
+    problems = {}
+
+    OK_ROOT_FILES = (
+        'LICENSE', 'UNLICENSE',
+        'README.markdown', 'TODO.markdown', 'HISTORY.markdown',
+        'test.sh', 'clean.sh',
+        'make.sh', 'make-cygwin.sh', 'Makefile',
+        '.hgtags', '.hgignore', '.gitignore',
+    )
+    OK_ROOT_DIRS = (
+        'bin', 'contrib', 'demo', 'dialect',
+        'disk', 'doc', 'ebin', 'eg', 'images',
+        'impl', 'lib', 'priv', 'script',
+        'src', 'tests',
+        '.hg',
+    )
+
+    def lint_it(source):
+        prob = []
+        os.chdir(source.dir)
+        if not os.path.exists('README.markdown'):
+            prob.append("No README.markdown")
+        if not os.path.exists('LICENSE') and not os.path.exists('UNLICENSE'):
+            prob.append("No LICENSE or UNLICENSE")
+        if os.path.exists('LICENSE') and os.path.exists('UNLICENSE'):
+            prob.append("Both LICENSE and UNLICENSE")
+        for root, dirnames, filenames in os.walk('.'):
+            if root.endswith(".hg"):
+                del dirnames[:]
+                continue
+            if root == '.':
+                root_files = []
+                for filename in filenames:
+                    if filename not in OK_ROOT_FILES:
+                        root_files.append(filename)
+                if root_files:
+                    prob.append(
+                        "Junk files in root: %s" % root_files
+                    )
+
+                root_dirs = []
+                for dirname in dirnames:
+                    if dirname not in OK_ROOT_DIRS:
+                        root_dirs.append(dirname)
+                if root_dirs:
+                    prob.append(
+                        "Junk dirs in root: %s" % root_dirs
+                    )
+        problems[source.dir] = prob
+
+    foreach_source(
+        expand_docked_specs(args), lint_it
+    )
+
+    problematic_count = 0
+    for d in sorted(problems.keys()):
+        if not problems[d]:
+            continue
+        print d
+        print '-' * len(d)
+        print
+        for problem in problems[d]:
+            print "* %s" % problem
+        print
+        problematic_count += 1
+
+    #print "Linted %d clones, problems in %d of them." % (
+    #    count, problematic_count
+    #)
 
 
 SUBCOMMANDS = {
@@ -1003,6 +1080,7 @@ SUBCOMMANDS = {
     'ghuser': ghuser,
     'bbuser': bbuser,
     'release': release,
+    'lint': lint,
 }
 
 
