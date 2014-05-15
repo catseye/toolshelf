518c18446eaf6522c09dfd48ed8e4e296aea2e83
Cat's Eye Technologies <cpressey@catseye.tc>
2014-05-15 13:07:46 +0100

    pushgh command, and updates to hg-git details.

---
+++ src/toolshelf/commands/pushgh.py
@@ -0,0 +1,16 @@
+import os
+
+def pushgh(shelf, args):
+    """Push updates to a Github git mirror of a Mercurial repository.  Requires
+    that the hg-git Mercurial extension has been set up.  Assumes that the name of
+    the repository on Github is the same as the project name (luckily, Github is
+    case insensitive about project names.)
+
+    """
+    def pushgh_it(source):
+        url = "git+ssh://git@github.com/%s/%s.git" % (source.user, source.project)
+        shelf.run('hg', 'push', url)
+
+    shelf.foreach_specced_source(
+        shelf.expand_docked_specs(args), pushgh_it
+    )
