8fe18ab53cfc31defbc48d588433ca725a0b937d
Chris Pressey <cpressey@catseye.tc>
2014-09-16 18:06:55 +0100

    Probably should've made this default the opposite.

--- src/toolshelf/commands/build.py
+++ src/toolshelf/commands/build.py
@@ -1,6 +1,9 @@
 from toolshelf.toolshelf import BaseCommand
 
 class Command(BaseCommand):
+    def show_progress(self):  # only if quiet
+        return False
+
     def perform(self, shelf, source):
         source.build()
         source.relink()
--- src/toolshelf/commands/update.py
+++ src/toolshelf/commands/update.py
@@ -1,6 +1,9 @@
 from toolshelf.toolshelf import BaseCommand
 
 class Command(BaseCommand):
+    def show_progress(self):
+        return False
+
     def perform(self, shelf, source):
         was_changed = source.update()
         if was_changed:
